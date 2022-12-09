37f64698835c3384cf9b6f6fdad3585cbcabc762
purplechickpea <alexandra.welp@web.de>
2022-12-09 16:24:01 +0100

    rearranged script, tidy up, rename some unclean variables

--- lab_setup_calculation.py
+++ lab_setup_calculation.py
@@ -13,96 +13,6 @@ import matplotlib.pyplot as plt
 import ht
 import fluid_properties_rp as rp
 
-fluid = "REFPROP::IsoButane[0.5]&Pentane[0.5]"
-
-p_o = 103000        # lowest possible pressure
-#T_o = CP.PropsSI("T", "P", p_o, "Q", 1, fluid)     # zeotropic fluid mixtures temperature for x = 1
-T_4c = CP.PropsSI("T", "P", p_o, "Q", 1, fluid)
-T_4b = CP.PropsSI("T", "P", p_o, "Q", 0, fluid)
-
-p_c = 13e5
-#T_c = CP.PropsSI("T", "P", p_c, "Q", 1, fluid)
-T_2c = CP.PropsSI("T", "P", p_c, "Q", 0, fluid)
-T_2b = CP.PropsSI("T", "P", p_c, "Q", 1, fluid)
-
-delta_T_4c_1 = 20          # delta T superheated low pressure
-eta_compressor = 0.65
-
-T_1 = (T_4c + delta_T_4c_1)
-h_1 = CP.PropsSI("H", "T", T_1, "P", p_o, fluid)
-s_1 = CP.PropsSI("S", "T", T_1, "P", p_o, fluid)
-h_2s = CP.PropsSI("H", "S", s_1, "P", p_c, fluid)
-
-h_2 = (h_2s - h_1) / eta_compressor + h_1
-T_2 = CP.PropsSI("T", "P", p_c, "H", h_2, fluid)
-s_2 = CP.PropsSI("S", "T", T_2, "P", p_c, fluid)
-
-w_compressor = h_2 - h_1
-T_2 = CP.PropsSI("T", "H", h_2, "P", p_c, fluid)
-delta_T_2_2b = T_2 - T_2b
-
-h_2b = CP.PropsSI("H", "P", p_c, "Q", 1, fluid)
-s_2b = CP.PropsSI("S", "P", p_c, "Q", 1, fluid)
-h_2c = CP.PropsSI("H", "P", p_c, "Q", 0, fluid)
-s_2c = CP.PropsSI("S", "P", p_c, "Q", 0, fluid)
-
-delta_h_sh = h_2 - h_2b
-delta_h_ws = h_2b - h_2c
-
-
-complete_subcooling = 'off'
-if complete_subcooling == 'on':
-    delta_T_4_4b = 10          # deltaT subcooled low pressure
-    T_4 = T_4b - delta_T_4_4b
-    h_4 = CP.PropsSI("H", "T", T_4, "P", p_o, fluid)
-    h_3 = h_4                   # throttle isenthalp
-    T_3 = CP.PropsSI("T", "P", p_c, "H", h_3, fluid)
-
-limited_subcooling = 'on'
-if limited_subcooling == 'on':
-    delta_Tu_T3 = 20
-    Tu = 25 + 273.15
-    T_3 = Tu + delta_Tu_T3
-    h_3 = CP.PropsSI("H", "T", T_3, "P", p_c, fluid)
-    h_4 = h_3
-    T_4 = CP.PropsSI("T", "H", h_4, "P", p_o, fluid)
-    if T_4 >= T_4b:
-        T_4b = T_4
-
-h_4b = CP.PropsSI("H", "P", p_o, "T", T_4b, fluid)
-s_4b = CP.PropsSI("S", "P", p_o, "T", T_4b, fluid)
-h_4c = CP.PropsSI("H", "P", p_o, "Q", 1, fluid)
-s_4c = CP.PropsSI("S", "P", p_o, "Q", 1, fluid)
-s_3 = CP.PropsSI("S", "T", T_3, "P", p_c, fluid)
-s_4 = CP.PropsSI("S", "P", p_o, "H", h_4, fluid)
-
-delta_h_sc = h_2c - h_3
-
-delta_h_o = h_1 - h_4
-
-
-
-
-print(f"delta_h superheated hp: {delta_h_sh} J/kg")
-print(f"delta_h wet steam hp: {delta_h_ws} J/kg")
-print(f"delta_h subcooled: {delta_h_sc} J/kg")
-
-
-# Calculation counter flow heat exchanger
-m_var = np.linspace(0.01, 0.025, 10)
-m_dot = 0.010
-rho_l = CP.PropsSI("D", "Q", 0, "P", p_c, fluid)
-rho_v = CP.PropsSI("D", "Q", 1, "P", p_c, fluid)
-mu_l = CP.PropsSI("VISCOSITY", "Q", 0, "P", p_c, fluid)
-mu_v = CP.PropsSI("VISCOSITY", "Q", 1, "P", p_c, fluid)
-k_l = CP.PropsSI("CONDUCTIVITY", "Q", 0, "P", p_c, fluid)
-cp_l = CP.PropsSI("C", "Q", 0, "P", p_c, fluid)
-cp_v = CP.PropsSI("C", "Q", 1, "P", p_c, fluid)
-p_crit = CP.PropsSI("PCRIT", fluid)
-d_i = 12e-3
-s = 1.5e-3
-d_a = 18e-3
-d_ai = d_i + 2 * s
 
 def T_log(T1ein, T1aus, T2ein, T2aus):
     deltaTein = T1ein - T2aus
@@ -113,58 +23,6 @@ def T_log(T1ein, T1aus, T2ein, T2aus):
         delta_T_log = (deltaTein - deltaTaus) / np.log(deltaTein / deltaTaus)
     return abs(delta_T_log)
 
-# secondary fluid
-
-T_pinch = 10
-T_sc_in = T_3 - T_pinch
-T_sc_out = T_2c - T_pinch
-T_sh_in = T_2b - T_pinch
-T_sh_out = T_2 - T_pinch
-T_ws_out = T_2b - 5
-delta_T_ws = 25
-T_ws_in = T_ws_out - delta_T_ws
-T_evap_out = T_4 + T_pinch
-T_evap_in = T_1 + T_pinch
-cp_w = 4181.9
-cp_methanol = CP.PropsSI("C", "T", 0.5 * (T_evap_in + T_evap_out), "P", 1e5, "REFPROP::methanol")
-
-
-
-# size of heat storages
-rho_w = 972.42      # density water
-timestorage = 3 * 3600
-rho_thermooil = 900
-cp_thermooil = 2300
-Q_storage_sc = m_dot * delta_h_sc * timestorage
-Q_storage_ws = m_dot * delta_h_ws * timestorage
-Q_storage_sh = m_dot * delta_h_sh * timestorage
-Q_storage_cold = m_dot * delta_h_o * timestorage
-
-delta_T_evap = T_evap_in - T_evap_out
-delta_T_sc = T_sc_out - T_sc_in
-
-delta_T_sh = T_sh_out - T_sh_in
-
-print(CP.PhaseSI( "T", 281, "D", 801, "Methanol"))
-rho_methanol = CP.PropsSI("D" ,"T", T_1, "P", 1e5, "Methanol")
-
-m_methanol = Q_storage_cold / (cp_methanol * delta_T_evap)
-m_sc = Q_storage_sc / (cp_w * delta_T_sc)
-m_ws = Q_storage_ws / (cp_w * delta_T_ws)
-m_sh = Q_storage_sh / (cp_thermooil * delta_T_sh)
-
-V_methanol = m_methanol / rho_methanol
-V_sc = m_sc / rho_w
-V_ws = m_ws / rho_w
-V_sh = m_sh / rho_thermooil
-
-print(f"Q_storage_methanol: {Q_storage_cold/1000} kJ = {Q_storage_cold/3600/1000} kWh, {V_methanol} m^3")
-print(f"Q_storage_sc: {Q_storage_sc/1000} kJ = {Q_storage_sc/3600/1000} kWh, {V_sc} m^3")
-print(f"Q_storage_ws: {Q_storage_ws/1000} kJ = {Q_storage_ws/3600/1000} kWh, {V_ws} m^3")
-print(f"Q_storage_sh: {Q_storage_sh/1000} kJ,= {Q_storage_sh/3600/1000} kWh, {V_sh} m^3")
-
-
-# calculate heat transfer coefficients
 
 def alpha_1P_i(p, T, fluid, m_dot, d_i):
     vis = CP.PropsSI("VISCOSITY", "P", p, "T", T, fluid)
@@ -198,14 +56,167 @@ def alpha_1P_annulus(p, T, fluid, m_dot, d_ai, d_a):
     return alpha
 
 
-m_sc_dot = m_dot * delta_h_sc / (cp_w * delta_T_sc)
+def cal_U(alpha_i, alpha_o, d_a, d_ai, lam_tube):
+    U = (1 / alpha_i + np.log(d_a / d_ai) * d_ai / (2 * lam_tube) + d_ai / (alpha_o * d_a)) ** (-1)
+    return U
+
+# input data for process
+fluid = "REFPROP::IsoButane[0.5]&Pentane[0.5]"
+
+p_o = 103000        # lowest possible pressure
+p_c = 13e5
+delta_T_4c_1 = 20          # delta T superheated low pressure
+eta_compressor = 0.65
+complete_subcooling = 'off'
+limited_subcooling = 'on'
+
+if complete_subcooling == limited_subcooling:
+    raise Exception("invalid input for subcooling, please choose one option")
+
+p_crit = CP.PropsSI("PCRIT", fluid)
+
+T_4c = CP.PropsSI("T", "P", p_o, "Q", 1, fluid)
+h_4c = CP.PropsSI("H", "P", p_o, "Q", 1, fluid)
+s_4c = CP.PropsSI("S", "P", p_o, "Q", 1, fluid)
+
+T_4b = CP.PropsSI("T", "P", p_o, "Q", 0, fluid)
+h_4b = CP.PropsSI("H", "P", p_o, "T", T_4b, fluid)
+s_4b = CP.PropsSI("S", "P", p_o, "T", T_4b, fluid)
+
+T_2c = CP.PropsSI("T", "P", p_c, "Q", 0, fluid)
+T_2b = CP.PropsSI("T", "P", p_c, "Q", 1, fluid)
+
+T_1 = (T_4c + delta_T_4c_1)
+h_1 = CP.PropsSI("H", "T", T_1, "P", p_o, fluid)
+s_1 = CP.PropsSI("S", "T", T_1, "P", p_o, fluid)
+h_2s = CP.PropsSI("H", "S", s_1, "P", p_c, fluid)
+
+h_2 = (h_2s - h_1) / eta_compressor + h_1
+T_2 = CP.PropsSI("T", "P", p_c, "H", h_2, fluid)
+s_2 = CP.PropsSI("S", "T", T_2, "P", p_c, fluid)
+
+w_compressor = h_2 - h_1
+delta_T_2_2b = T_2 - T_2b
+
+h_2b = CP.PropsSI("H", "P", p_c, "Q", 1, fluid)
+s_2b = CP.PropsSI("S", "P", p_c, "Q", 1, fluid)
+rho_2b = CP.PropsSI("D", "Q", 1, "P", p_c, fluid)
+mu_2b = CP.PropsSI("VISCOSITY", "Q", 1, "P", p_c, fluid)
+cp_2b = CP.PropsSI("C", "Q", 1, "P", p_c, fluid)
+
+
+h_2c = CP.PropsSI("H", "P", p_c, "Q", 0, fluid)
+s_2c = CP.PropsSI("S", "P", p_c, "Q", 0, fluid)
+rho_2c = CP.PropsSI("D", "Q", 0, "P", p_c, fluid)
+mu_2c = CP.PropsSI("VISCOSITY", "Q", 0, "P", p_c, fluid)
+k_2c = CP.PropsSI("CONDUCTIVITY", "Q", 0, "P", p_c, fluid)
+cp_2c = CP.PropsSI("C", "Q", 0, "P", p_c, fluid)
+
+
+
+if complete_subcooling == 'on':
+    delta_T_4_4b = 10          # deltaT subcooled low pressure
+    T_4 = T_4b - delta_T_4_4b
+    h_4 = CP.PropsSI("H", "T", T_4, "P", p_o, fluid)
+    h_3 = h_4                   # throttle isenthalp
+    T_3 = CP.PropsSI("T", "P", p_c, "H", h_3, fluid)
+
+
+if limited_subcooling == 'on':
+    delta_Tu_T3 = 20
+    Tu = 25 + 273.15
+    T_3 = Tu + delta_Tu_T3
+    h_3 = CP.PropsSI("H", "T", T_3, "P", p_c, fluid)
+    h_4 = h_3
+    T_4 = CP.PropsSI("T", "H", h_4, "P", p_o, fluid)
+    if T_4 >= T_4b:
+        T_4b = T_4
+
+
+s_3 = CP.PropsSI("S", "T", T_3, "P", p_c, fluid)
+s_4 = CP.PropsSI("S", "P", p_o, "H", h_4, fluid)
+
+delta_h_sc = h_2c - h_3
+
+delta_h_sh = h_2 - h_2b
+delta_h_ws = h_2b - h_2c
+delta_h_o = h_1 - h_4
+
+print(f"delta_h superheated hp: {delta_h_sh} J/kg")
+print(f"delta_h wet steam hp: {delta_h_ws} J/kg")
+print(f"delta_h subcooled: {delta_h_sc} J/kg")
+
+
+# Calculation counter flow heat exchanger
+m_dot = 0.010
+d_i = 12e-3
+s = 1.5e-3
+d_a = 18e-3
+d_ai = d_i + 2 * s
+
+# secondary fluid
+
+T_pinch = 10
+T_sc_in = T_3 - T_pinch
+T_sc_out = T_2c - T_pinch
+T_sh_in = T_2b - T_pinch
+T_sh_out = T_2 - T_pinch
+T_ws_out = T_2b - 5
+delta_T_ws = 25
+T_ws_in = T_ws_out - delta_T_ws
+T_evap_out = T_4 + T_pinch
+T_evap_in = T_1 + T_pinch
+cp_ws_water = CP.PropsSI("C", "T", 0.5 * (T_ws_out + T_ws_in), "P", 1e5, "REFPROP::water")
+rho_ws_water = CP.PropsSI("D", "T", 0.5 * (T_ws_out + T_ws_in), "P", 1e5, "REFPROP::water")
+cp_sc_water = CP.PropsSI("C", "T", 0.5 * (T_sc_out + T_sc_in), "P", 1e5, "REFPROP::water")
+rho_sc_water = CP.PropsSI("D", "T",0.5 * (T_sc_out + T_sc_in), "P", 1e5, "REFPROP::water")
+cp_methanol = CP.PropsSI("C", "T", 0.5 * (T_evap_in + T_evap_out), "P", 1e5, "REFPROP::methanol")
+rho_methanol = CP.PropsSI("D", "T", 0.5 * (T_evap_in + T_evap_out), "P", 1e5, "REFPROP::Methanol")
+
+# size of heat storages
+timestorage = 3 * 3600
+rho_thermooil = 900
+cp_thermooil = 2300
+Q_storage_sc = m_dot * delta_h_sc * timestorage
+Q_storage_ws = m_dot * delta_h_ws * timestorage
+Q_storage_sh = m_dot * delta_h_sh * timestorage
+Q_storage_cold = m_dot * delta_h_o * timestorage
+
+delta_T_evap = T_evap_in - T_evap_out
+delta_T_sc = T_sc_out - T_sc_in
+
+delta_T_sh = T_sh_out - T_sh_in
+
+
+m_methanol = Q_storage_cold / (cp_methanol * delta_T_evap)
+m_sc = Q_storage_sc / (cp_sc_water * delta_T_sc)
+m_ws = Q_storage_ws / (cp_ws_water * delta_T_ws)
+m_sh = Q_storage_sh / (cp_thermooil * delta_T_sh)
+
+V_methanol = m_methanol / rho_methanol
+V_sc = m_sc / rho_sc_water
+V_ws = m_ws / rho_ws_water
+V_sh = m_sh / rho_thermooil
+
+print(f"Q_storage_methanol: {Q_storage_cold/1000} kJ = {Q_storage_cold/3600/1000} kWh, {V_methanol} m^3")
+print(f"Q_storage_sc: {Q_storage_sc/1000} kJ = {Q_storage_sc/3600/1000} kWh, {V_sc} m^3")
+print(f"Q_storage_ws: {Q_storage_ws/1000} kJ = {Q_storage_ws/3600/1000} kWh, {V_ws} m^3")
+print(f"Q_storage_sh: {Q_storage_sh/1000} kJ,= {Q_storage_sh/3600/1000} kWh, {V_sh} m^3")
+
+
+# calculate heat transfer coefficients
+
+
+
+
+m_sc_dot = m_dot * delta_h_sc / (cp_sc_water * delta_T_sc)
 m_sh_dot = m_dot * delta_h_sh / (cp_thermooil * delta_T_sh)
-m_ws_dot = m_dot * delta_h_ws / (cp_w * delta_T_ws)
+m_ws_dot = m_dot * delta_h_ws / (cp_ws_water * delta_T_ws)
 m_evap = m_dot * delta_h_o / (cp_methanol * delta_T_evap)
 
-u_sc = m_sc_dot / (rho_w * np.pi * 0.25 * (d_a ** 2 - d_ai ** 2))
-u_ws = m_ws_dot / (rho_w * np.pi * 0.25 * (d_a ** 2 - d_ai ** 2))
-u_wf = m_dot / (rho_v * np.pi * 0.25 * d_i ** 2)
+u_sc = m_sc_dot / (rho_sc_water * np.pi * 0.25 * (d_a ** 2 - d_ai ** 2))
+u_ws = m_ws_dot / (rho_ws_water * np.pi * 0.25 * (d_a ** 2 - d_ai ** 2))
+u_wf = m_dot / (rho_2b * np.pi * 0.25 * d_i ** 2)
 u_sh = m_sh_dot / (935 * np.pi * 0.25 * (d_a ** 2 - d_ai ** 2))
 u_evap = m_evap / (rho_methanol * np.pi * 0.25 * (d_a ** 2 - d_ai ** 2))
 
@@ -214,7 +225,7 @@ alpha_sh_i = alpha_1P_i(p_c, 0.5 * (T_2b + T_2), fluid, m_dot, d_i)
 alpha_sc_o = alpha_1P_annulus(1e5, 0.5 * (T_sc_in + T_sc_out), 'water', m_sc_dot, d_ai, d_a)
 alpha_sh_o = alpha_1P_annulus(1e5, T_2, 'thermooil', m_sh_dot, d_ai, d_a)
 alpha_ws_o = alpha_1P_annulus(1e5, 0.5 * (T_ws_in + T_ws_out), 'water', m_ws_dot, d_ai, d_a)
-alpha_ws_i = ht.condensation.Shah(m_dot, 0.5, d_i, rho_l, mu_l, k_l, cp_l, p_c, p_crit)
+alpha_ws_i = ht.condensation.Shah(m_dot, 0.5, d_i, rho_2c, mu_2c, k_2c, cp_2c, p_c, p_crit)
 
 rho_l_evap = CP.PropsSI("D", "P", p_o, "Q", 0, fluid)
 rho_v_evap = CP.PropsSI("D", "P", p_o, "Q", 1, fluid)
@@ -232,9 +243,7 @@ alpha_evap_i = ht.boiling_flow.Chen_Bennett(m_dot, 0.5, d_i, rho_l_evap, rho_v_e
 alpha_evap_o = alpha_1P_annulus(1e5, 0.5 * (T_evap_in + T_evap_out), 'methanol', m_evap, d_ai, d_a)
 
 
-def cal_U(alpha_i, alpha_o, d_a, d_ai, lam_tube):
-    U = (1 / alpha_i + np.log(d_a / d_ai) * d_ai / (2 * lam_tube) + d_ai / (alpha_o * d_a)) ** (-1)
-    return U
+
 
 
 lam_tube = 300
