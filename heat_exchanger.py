9e15d8250f6d5e71c3197531a9ea85c974095958
purplechickpea <alexandra.welp@uni-due.de>
2023-03-31 11:26:49 +0200

    not converges yet

--- heat_exchanger.py
+++ heat_exchanger.py
@@ -48,11 +48,16 @@ def call_HE_co(T_KM_ein, T_W_out, p_KM, p_W, resolution, di, alpha_local, m_dot_
     res = solve_bvp(lambda x, h: heat_exchanger_co_current(x, h, p_KM, p_W, fluid_1, fluid_2, comp,
                                                            di, alpha_local, m_dot_1, m_dot_2),
                     bc_he_co, x_var, h_schaetz)
-    if res.message != "The algorithm converged to the desired accuracy.":
-        print(res.message)
-        raise ValueError("The solver did not converge.")
+
     T_KM_aus = fprop.hp(res.y[0,-1], p_KM, fluid_1, comp)[0]
     T_W_ein = fprop.hp(res.y[1,0], p_W, fluid_2)[0]
+    if res.message != "The algorithm converged to the desired accuracy.":
+        print(res.message)
+        print(f"T_KM_ein={T_KM_ein}, T_W_out={T_W_out}")
+        print(f"h_KM_ein={h1_ein}, h_W_out={h2_out}")
+        print(f"Enthalpien aus Solver: {res.y[0,-1]}, {res.y[1,-1]}")
+        print(f"Temperaturen aus Solver: {T_KM_aus}, {T_W_ein}")
+        raise ValueError("The solver did not converge.")
     return T_KM_aus, T_W_ein
 
 def linked_he(T_W9, T_KM1, T_W1_real, p_KM, p_W, fluid_1, fluid_2, comp, resolution):
@@ -75,7 +80,7 @@ def linked_he(T_W9, T_KM1, T_W1_real, p_KM, p_W, fluid_1, fluid_2, comp, resolut
     plt.plot(np.linspace(0,16,9), [T_KM1, T_KM2, T_KM3, T_KM4, T_KM5, T_KM6, T_KM7, T_KM8, T_KM9],'*k')
     plt.plot(np.linspace(0,14,8), [T_W8, T_W7, T_W6, T_W5, T_W4, T_W3, T_W2, T_W1],'*b')
     plt.plot(np.linspace(2, 16, 8), [T_W9, T_W8, T_W7, T_W6, T_W5, T_W4, T_W3, T_W2],'*r')
-
+    #plt.show()
     return T_W1 - T_W1_real
 
 def get_overall_temperatures(T_KM_ein, T_W_out, p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1, fluid_2, comp):
@@ -102,45 +107,47 @@ def get_overall_temperatures(T_KM_ein, T_W_out, p_KM, p_W, resolution, di, alpha
     if res.message != "The algorithm converged to the desired accuracy.":
         print(res.message)
         raise ValueError("The solver did not converge.")
+    h_KM = res.y[0]
+    h_W = res.y[1]
     T_KM_aus = fprop.hp_v(res.y[0], p_KM, fluid_1, comp)[0]
     T_W_ein = fprop.hp_v(res.y[1], p_W, fluid_2)[0]
-    return T_KM_aus, T_W_ein
+    return T_KM_aus, T_W_ein, h_KM, h_W
 
 
 if __name__ == "__main__":
     fluid_1 = "Propane;Isobutane"
     comp = [0.4, 0.6]
     fluid_2 = "water"
-    m_dot_1 = 20e-3
-    m_dot_2 = 20e-3
-    p_KM = 10e6
-    p_W = 1e6
+    m_dot_1 = 10e-3
+    m_dot_2 = 10e-3
+    p_KM = 6e5
+    p_W = 1e5
 
     alpha_local = 400
     di = 10e-3
     l = 2
-    T_KM1 = 55 + 273.15
-    T_W1_real = 20 +273.15
-    T_W9_guess = 45+ 273.15
+    T_KM1 = 34 + 273.15
+    T_W1_real = 4 +273.15
+    T_W9_guess = 30 + 273.15
     resolution = 100
 
     res = root(linked_he, T_W9_guess, args=(T_KM1, T_W1_real, p_KM, p_W, fluid_1, fluid_2, comp, resolution))
     T_W9 = res.x
 
-    T_KM_R1, T_W_R1 = get_overall_temperatures(T_KM1, T_W9, p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1, fluid_2, comp)
-    T_KM_R2, T_W_R2 = get_overall_temperatures(T_KM_R1[-1], T_W_R1[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
+    T_KM_R1, T_W_R1, h_KM_R1, h_W_R1 = get_overall_temperatures(T_KM1, T_W9, p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1, fluid_2, comp)
+    T_KM_R2, T_W_R2, h_KM_R2, h_W_R2 = get_overall_temperatures(T_KM_R1[-1], T_W_R1[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
                              fluid_2, comp)
-    T_KM_R3, T_W_R3 = get_overall_temperatures(T_KM_R2[-1], T_W_R2[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
+    T_KM_R3, T_W_R3, h_KM_R3, h_W_R3 = get_overall_temperatures(T_KM_R2[-1], T_W_R2[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
                              fluid_2, comp)
-    T_KM_R4, T_W_R4 = get_overall_temperatures(T_KM_R3[-1], T_W_R3[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
+    T_KM_R4, T_W_R4, h_KM_R4, h_W_R4 = get_overall_temperatures(T_KM_R3[-1], T_W_R3[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
                              fluid_2, comp)
-    T_KM_R5, T_W_R5 = get_overall_temperatures(T_KM_R4[-1], T_W_R4[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
+    T_KM_R5, T_W_R5, h_KM_R5, h_W_R5 = get_overall_temperatures(T_KM_R4[-1], T_W_R4[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
                              fluid_2, comp)
-    T_KM_R6, T_W_R6 = get_overall_temperatures(T_KM_R5[-1], T_W_R5[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
+    T_KM_R6, T_W_R6, h_KM_R6, h_W_R6 = get_overall_temperatures(T_KM_R5[-1], T_W_R5[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
                              fluid_2, comp)
-    T_KM_R7, T_W_R7 = get_overall_temperatures(T_KM_R6[-1], T_W_R6[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
+    T_KM_R7, T_W_R7, h_KM_R7, h_W_R7 = get_overall_temperatures(T_KM_R6[-1], T_W_R6[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
                              fluid_2, comp)
-    T_KM_R8, T_W_R8 = get_overall_temperatures(T_KM_R7[-1], T_W_R7[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
+    T_KM_R8, T_W_R8, h_KM_R8, h_W_R8 = get_overall_temperatures(T_KM_R7[-1], T_W_R7[0], p_KM, p_W, resolution, di, alpha_local, m_dot_1, m_dot_2, fluid_1,
                              fluid_2, comp)
 
     plt.figure(2)
@@ -165,8 +172,40 @@ if __name__ == "__main__":
     plt.plot(np.linspace(12, 14, resolution), T_KM_R7, '-b')
     plt.plot(np.linspace(12, 14, resolution), T_W_R7, '-r')
 
-    plt.plot(np.linspace(14, 16, resolution), T_KM_R8, '-b')
-    plt.plot(np.linspace(14, 16, resolution), T_W_R8, '-r')
+    plt.plot(np.linspace(14, 16, resolution), T_KM_R8, '-b', label="working fluid")
+    plt.plot(np.linspace(14, 16, resolution), T_W_R8, '-r', label="water")
 
+    plt.xlabel("length in m")
+    plt.ylabel("T in K")
+    plt.legend()
+
+    plt.figure(3)
+    plt.plot(np.linspace(0, 2, resolution), h_KM_R1, '-b')
+    plt.plot(np.linspace(0, 2, resolution), h_W_R1, '-r')
+
+    plt.plot(np.linspace(2, 4, resolution), h_KM_R2, '-b')
+    plt.plot(np.linspace(2, 4, resolution), h_W_R2, '-r')
+
+    plt.plot(np.linspace(4, 6, resolution), h_KM_R3, '-b')
+    plt.plot(np.linspace(4, 6, resolution), h_W_R3, '-r')
+
+    plt.plot(np.linspace(6, 8, resolution), h_KM_R4, '-b')
+    plt.plot(np.linspace(6, 8, resolution), h_W_R4, '-r')
+
+    plt.plot(np.linspace(8, 10, resolution), h_KM_R5, '-b')
+    plt.plot(np.linspace(8, 10, resolution), h_W_R5, '-r')
+
+    plt.plot(np.linspace(10, 12, resolution), h_KM_R6, '-b')
+    plt.plot(np.linspace(10, 12, resolution), h_W_R6, '-r')
+
+    plt.plot(np.linspace(12, 14, resolution), h_KM_R7, '-b')
+    plt.plot(np.linspace(12, 14, resolution), h_W_R7, '-r')
+
+    plt.plot(np.linspace(14, 16, resolution), h_KM_R8, '-b', label="working fluid")
+    plt.plot(np.linspace(14, 16, resolution), h_W_R8, '-r', label="water")
+
+    plt.xlabel("length in m")
+    plt.ylabel("h in J/kg")
+    plt.legend()
     plt.show()
 
